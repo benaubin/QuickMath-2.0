81404949bbd6349333fc38986c22232de0152863
Ben! <penne12@allforfunmc.com>
2014-11-18 08:57:20 -0600

    jdoc

--- src/com/bensites/java/QuickMath/core/Operators/ppm.groovy
+++ src/com/bensites/java/QuickMath/core/Operators/ppm.groovy
@@ -2,6 +2,11 @@ package com.bensites.java.QuickMath.core.Operators;
 
 import com.bensites.java.QuickMath.core.Operator
 
+/**
+ * Parts per million
+ * @author Ben
+ *
+ */
 public class ppm extends Operator{
 
 	public ppm() {
